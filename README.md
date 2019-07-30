4de5950fd4d0c4091fe7298e709ad6afad27b33c
Chamver <johnc@ubuntu.local>
2019-07-30 17:59:43 +0800

    BOS React Challenge Completed

--- README.md
+++ README.md
@@ -1,44 +1,19 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## React Coding Challenge
 
-## Available Scripts
+Use the following technology to create an web application:
+    Typescript
+    React (Latest version)
+    Material-UI
+    NPM
+    MomentJS
 
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+Requirements:
+    Create a new project using Create React App
+    Read data from RandomAPI and display in a table (10 results)
+    Use Material-UI for React for the UI
+    Display basic data such as Name, Address, DOB, Gender and Country
+    Display a user's DOB in format "28 years ago" with MomentJS
+    Add a "Refresh" button to get a new date from the API
+    Allow searching in text input through the current data within the table. Use custom implementation for this, do not use Material-UI to handle it
+    Make both Date and Name columns sortable. Use custom implementation for this, do not use Material-UI to handle it
+    Make sure to use good use of components throughtout the application
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,145 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import moment from "moment";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TextField from "@material-ui/core/TextField";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+interface RandomApi {
+  name: string,
+  address: string,
+  birthday: string,
+  gender: string,
+  country: string
 }
 
-export default App;
+export default class App extends Component {
+
+  state = { rows: { data: Array<RandomApi>() }, copy: { data: Array<RandomApi>() }, sortDirection: "ASC", loading: true }
+
+  componentDidMount() {
+    fetch(`https://randomapi.com/api/s8plzshd?key=JQKX-0TJI-QH86-6ERM`)
+      .then(response => response.json())
+      .then(({ results }) => {
+        const data = results[0];
+        this.setState({ rows: data, copy: data, loading: false })
+      });
+  }
+
+  getNewData = () => {
+    fetch(`https://randomapi.com/api/s8plzshd?key=JQKX-0TJI-QH86-6ERM`)
+      .then(response => response.json())
+      .then(({ results }) => {
+        const data = results[0];
+        this.setState({ rows: data, copy: data, loading: false })
+      });
+  }
+
+  filterData = (event: React.ChangeEvent) => {
+    event.preventDefault()
+    const input = event.target as HTMLInputElement
+    const { rows: { data }, copy: { data: previous } } = this.state
+    const newData = data.filter(d => d.name.includes(input.value))
+    if (input.value) {
+      this.setState({ rows: { data: newData } })
+    } else {
+      this.setState({ rows: { data: previous } })
+    }
+  }
+
+  handleSort = (sort: string) => {
+    const { rows: { data }, sortDirection } = this.state
+    let newData = []
+    if (sortDirection === "ASC") {
+      this.setState({ sortDirection: "DESC" })
+      newData = data.sort((a: any, b: any) => (a[sort] < b[sort]) ? 1 : -1)
+    } else {
+      this.setState({ sortDirection: "ASC" })
+      newData = data.sort((a: any, b: any) => (a[sort] > b[sort]) ? 1 : -1)
+    }
+    this.setState({ rows: { data: newData } })
+  }
+
+  render() {
+    const { rows: { data }, loading } = this.state;
+
+    return data.length > -1 ?
+      <div style={bodyStyles}>
+        <div style={headerStyles}>
+          <Button variant="contained" onClick={this.getNewData}>
+            Refresh
+          </Button>
+          <div style={{ width: "30%" }}>
+            <TextField
+              label="Search"
+              id="outlined-bare"
+              placeholder="Search for a name here"
+              margin="normal"
+              variant="outlined"
+              inputProps={{ "aria-label": "bare" }}
+              onChange={this.filterData}
+              fullWidth={true}
+            />
+          </div>
+        </div>
+        <Paper>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell onClick={() => this.handleSort("name")}>Name</TableCell>
+                <TableCell>Address</TableCell>
+                <TableCell onClick={() => this.handleSort("birthday")}>Birthday</TableCell>
+                <TableCell>Gender</TableCell>
+                <TableCell>Country</TableCell>
+              </TableRow>
+            </TableHead>
+            {!loading ?
+              <TableBody>
+                {data.map((d, index) =>
+                  <TableRow key={index}>
+                    <TableCell component="th" scope="row">
+                      {d.name}
+                    </TableCell>
+                    <TableCell>{d.address}</TableCell>
+                    <TableCell>{moment().diff(d.birthday, 'years') + " years ago"}</TableCell>
+                    <TableCell>{d.gender}</TableCell>
+                    <TableCell>{d.country}</TableCell>
+                  </TableRow>
+                )}
+              </TableBody> : <TableBody>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell align="center"><CircularProgress /></TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+              </TableBody>}
+          </Table>
+        </Paper>
+      </div>
+      : <div><h1 style={loadingStyles}>Table is still loading. . .</h1><LinearProgress color="secondary" /></div>
+  };
+}
+
+const headerStyles = {
+  display: "flex",
+  justifyContent: "space-evenly",
+  alignItems: "center",
+  marginBottom: "20px"
+}
+
+const bodyStyles = {
+  marginTop: "20px",
+  marginBottom: "20px"
+}
+
+const loadingStyles = {
+  textAlign: "center" as "center"
+}
